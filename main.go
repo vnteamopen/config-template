c3799b0f1f987ba6596019da2c9acf61f8110d2c
Thuc Le <ledongthuc@gmail.com>
2022-07-01 20:28:48 +0200

    refactoring code

--- main.go
+++ main.go
@@ -39,6 +39,7 @@ func main() {
 		HelpName: "config-template",
 		Usage:    "A tool to merge file's contents to a template. Embedded pattern is {{file \"\"}}",
 		UsageText: `config-template /path/to/input/file /path/to/output/file
+config-template -w /path/to/input/file
 config-template help`,
 		EnableBashCompletion: true,
 		Flags:                Flags,
@@ -50,36 +51,17 @@ config-template help`,
 func Action(c *cli.Context) error {
 	c.App.Setup()
 
-	requiredArgs := 2
 	isOverwrite := c.Bool(string(FlagOverwrite))
-	if isOverwrite {
-		requiredArgs = 1
-	}
-
-	if c.NArg() < requiredArgs {
+	valid, requiredArgs := validArgs(c.NArg(), isOverwrite)
+	if !valid {
 		cli.ShowAppHelp(c)
-		return cli.Exit("", 0)
+		return cli.Exit("", 1)
 	}
+	templatePath, outputPaths := getPaths(c.Args(), requiredArgs, isOverwrite)
 
-	templatePath := c.Args().Get(0)
-	numberOfAdditionOutput := c.NArg() - requiredArgs
-
-	listOutputPath := make([]string, 0, c.NArg())
-	if isOverwrite {
-		listOutputPath = append(listOutputPath, actions.CreateTmpFile(templatePath))
-	} else {
-		firstOutPut := c.Args().Get(requiredArgs - 1)
-		listOutputPath = append(listOutputPath, firstOutPut)
-	}
-
-	for i := 0; i < numberOfAdditionOutput; i++ {
-		listOutputPath = append(listOutputPath, c.Args().Get(requiredArgs+i))
-	}
-
-	if err := actions.CharByCharMerge(templatePath, listOutputPath); err != nil {
+	if err := actions.CharByCharMerge(templatePath, outputPaths); err != nil {
 		return cli.Exit(err.Error(), 1)
 	}
-
 	if isOverwrite {
 		if err := actions.OverwriteInput(templatePath); err != nil {
 			return cli.Exit(err.Error(), 1)
@@ -87,4 +69,33 @@ func Action(c *cli.Context) error {
 	}
 
 	return cli.Exit("", 0)
+}
+
+func validArgs(totalArgs int, isOverwrite bool) (valid bool, requiredArgs int) {
+	requiredArgs = 2
+	if isOverwrite {
+		requiredArgs = 1
+	}
+
+	return totalArgs >= requiredArgs, requiredArgs
+}
+
+func getPaths(args cli.Args, requiredArgs int, isOverwrite bool) (templatePath string, listOutputPath []string) {
+	totalArgs := args.Len()
+	templatePath = args.Get(0)
+	numberOfAdditionOutput := totalArgs - requiredArgs
+
+	listOutputPath = make([]string, 0, totalArgs)
+	if isOverwrite {
+		listOutputPath = append(listOutputPath, actions.CreateTmpFile(templatePath))
+	} else {
+		firstOutPut := args.Get(requiredArgs - 1)
+		listOutputPath = append(listOutputPath, firstOutPut)
+	}
+
+	for i := 0; i < numberOfAdditionOutput; i++ {
+		listOutputPath = append(listOutputPath, args.Get(requiredArgs+i))
+	}
+
+	return templatePath, listOutputPath
 }
